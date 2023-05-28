4b6effb3e93f918293ae9faec7bb8c6cbacc2ffc
ckboa <48851053+ckboa@users.noreply.github.com>
2023-05-28 10:32:33 +0700

    Update start.py

--- app/start.py
+++ app/start.py
@@ -1,1 +1,26 @@
-print('Version 2 installed using USB') 
+from machine import Pin
+import random, ujson, utime
+import dht
+import gc
+
+gc.collect() 
+
+
+sensor = dht.DHT22(Pin(16))
+
+while True:
+    temperature_dht = 35
+    humidity_dht = 50
+    fahrenheit = 100
+    temperature_esp32 = (fahrenheit - 32.0)/1.8
+    print("Temperature (DHT): {}C, Humidity (DHT): {}%, Temperature (ESP32): {:.2f}C".format(
+        temperature_dht, humidity_dht, temperature_esp32))
+    
+    payload = ujson.dumps({
+        'temperature': temperature_dht,
+        'humidity': humidity_dht,
+        'esp32Temperature': temperature_esp32
+    })
+    
+    gc.collect()
+    utime.sleep(3)
